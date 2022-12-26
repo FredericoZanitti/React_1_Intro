b2c0799fd4b765edcbd12433f7f6c08ffdcd69cd
FredericoZanitti <fredzanitti@gmail.com>
2022-12-26 16:10:12 +0000

    Introdução ao React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HELLO WORLD REACT</h1>
+      <h2>Emmet Funcionou!</h2>
+      <h3>Frederico Zanitti</h3>
+    </div>
+  );
+}
+
+export default App;
